05febf9760d5bef5685f063d3caf4420912a8b43
V-i-c-t-o-r1 <victorverkoelen@hotmail.com>
2021-04-09 10:55:01 +0200

    Working PWA

--- Connfy/service-worker.js
+++ Connfy/service-worker.js
@@ -1,5 +1,25 @@
+const cacheName="Main_Page";
+const appFiles=[
+  "/",
+	"manifest.json",
+	"js/script.js",
+	"css/stylesheet.css",
+	"images/icon-512.png",
+  "images/icon-192.png"
+];
+
 self.addEventListener("install",(installing)=>{
     console.log("Service Worker: I am being installed, hello world!");
+    //Put important offline files in cache on installation of the service worker
+installing.waitUntil(
+  caches.open(cacheName).then((cache)=>{
+    console.log("Service Worker: Caching important offline files");
+    return cache.addAll(appFiles);
+  })
+);
+
+
+
   });
   
   self.addEventListener("activate",(activating)=>{	
@@ -8,6 +28,26 @@ self.addEventListener("install",(installing)=>{
   
   self.addEventListener("fetch",(fetching)=>{   
     console.log("Service Worker: User threw a ball, I need to fetch it!");
+
+
+    fetching.respondWith(
+      caches.match(fetching.request.url).then((response)=>{
+        console.log("Service Worker: Fetching resource "+fetching.request.url);
+        return response||fetch(fetching.request).then((response)=>{
+          console.log("Service Worker: Resource "+fetching.request.url+" not available in cache");
+          return caches.open(cacheName).then((cache)=>{
+              console.log("Service Worker: Caching (new) resource "+fetching.request.url);
+              cache.put(fetching.request,response.clone());
+            return response;
+          });
+        }).catch(function(){      
+          console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
+          //Do something else with the request (respond with a different cached file)
+        })
+      })
+    );
+
+
   });
   
   self.addEventListener("push",(pushing)=>{
