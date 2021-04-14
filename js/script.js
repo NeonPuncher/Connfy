75dac24f0dd085e000ec784123d389d1c6782cc3
Britt <b.zegveld@student.fontys.nl>
2021-04-14 22:00:14 +0200

    Added custom install button with IOS compatibility to PWA and minor bug fixes

--- js/script.js
+++ js/script.js
@@ -11,3 +11,69 @@ if("serviceWorker" in navigator){
     //The registration of the service worker failed
     console.log("Browser: I don't support Service Workers :(");
   }
+
+
+let installPrompt; //Variable to store the install action in
+let button = document.getElementById("installButton"); //Variable to store html button in
+
+window.addEventListener("beforeinstallprompt",(event)=>{	
+  event.preventDefault(); //Prevent the event (this prevents the default bar to show up)
+  installPrompt=event; //Install event is stored for triggering it later
+  button.style.display = "block"; //Button is now visible
+  console.log("App is niet geïnstalleerd, knop is zichtbaar");
+});
+
+button.addEventListener("click", (installApp)=>{
+  installPrompt.prompt();
+  button.style.display = "none"; //Button is now invisible
+  console.log("App is aangeklikt, knop is onzichtbaar");
+
+  installPrompt.userChoice.then((choiceResult)=>{
+    button.style.display = "none"; //Button is now invisible
+    console.log("Installatie is geaccepteerd, knop blijft onzichtbaar");
+
+    if(choiceResult.outcome!=="accepted"){
+      //..If it was not accepted to install than show the install button again
+      button.style.display = "block"; //Button is now visible
+      console.log("Installatie is geweigerd, knop is weer zichtbaar");
+    }
+    installPrompt=null;
+  });
+});
+
+
+//iOS install tip show
+const isIOSUsed=()=>{
+  const userAgent=window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test(userAgent); //Return if iOS is used (iPhone, iPod or iPad)
+}
+const standaloneModeActive=()=>("standalone" in window.navigator)&&(window.navigator.standalone); //Will be true if the PWA is used
+if(isIOSUsed()&&!standaloneModeActive()){ 
+  //Show your install tip for iOS here
+  console.log("IOS button exists");
+  
+  window.addEventListener("beforeinstallprompt",(event)=>{	
+    event.preventDefault(); //Prevent the event (this prevents the default bar to show up)
+    installPrompt=event; //Install event is stored for triggering it later
+    button.style.display = "block"; //Button is now visible
+    console.log("App is niet geïnstalleerd, knop is zichtbaar");
+  });
+  
+  button.addEventListener("click", (installApp)=>{
+    installPrompt.prompt();
+    button.style.display = "none"; //Button is now invisible
+    console.log("App is aangeklikt, knop is onzichtbaar");
+
+    installPrompt.userChoice.then((choiceResult)=>{
+      button.style.display = "none"; //Button is now invisible
+      console.log("Installatie is geaccepteerd, knop blijft onzichtbaar");
+
+      if(choiceResult.outcome!=="accepted"){
+        //..If it was not accepted to install than show the install button again
+        button.style.display = "block"; //Button is now visible
+        console.log("Installatie is geweigerd, knop is weer zichtbaar");
+      }
+      installPrompt=null;
+    });
+  });
+}
--- service-worker.js
+++ service-worker.js
@@ -22,48 +22,40 @@ const appFiles=[
 ];
 
 self.addEventListener("install",(installing)=>{
-    console.log("Service Worker: I am being installed, hello world!");
-    //Put important offline files in cache on installation of the service worker
-installing.waitUntil(
-  caches.open(cacheName).then((cache)=>{
-    console.log("Service Worker: Caching important offline files");
-    return cache.addAll(appFiles);
-  })
-);
-
-
-
-  });
+  console.log("Service Worker: I am being installed, hello world!");
+  //Put important offline files in cache on installation of the service worker
+  installing.waitUntil(
+    caches.open(cacheName).then((cache)=>{
+      console.log("Service Worker: Caching important offline files");
+      return cache.addAll(appFiles);
+    })
+  );
+});
   
-  self.addEventListener("activate",(activating)=>{	
-    console.log("Service Worker: All systems online, ready to go!");
-  });
+self.addEventListener("activate",(activating)=>{	
+  console.log("Service Worker: All systems online, ready to go!");
+});
   
-  self.addEventListener("fetch",(fetching)=>{   
-    console.log("Service Worker: User threw a ball, I need to fetch it!");
-
-
-    fetching.respondWith(
-      caches.match(fetching.request.url).then((response)=>{
-        console.log("Service Worker: Fetching resource "+fetching.request.url);
-        return response||fetch(fetching.request).then((response)=>{
-          console.log("Service Worker: Resource "+fetching.request.url+" not available in cache");
-          return caches.open(cacheName).then((cache)=>{
-              console.log("Service Worker: Caching (new) resource "+fetching.request.url);
-              cache.put(fetching.request,response.clone());
-            return response;
-          });
-        }).catch(function(){      
-          console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
-          //Do something else with the request (respond with a different cached file)
-        })
+self.addEventListener("fetch",(fetching)=>{   
+  console.log("Service Worker: User threw a ball, I need to fetch it!");
+  fetching.respondWith(
+    caches.match(fetching.request.url).then((response)=>{
+      console.log("Service Worker: Fetching resource "+fetching.request.url);
+      return response||fetch(fetching.request).then((response)=>{
+        console.log("Service Worker: Resource "+fetching.request.url+" not available in cache");
+        return caches.open(cacheName).then((cache)=>{
+          console.log("Service Worker: Caching (new) resource "+fetching.request.url);
+          cache.put(fetching.request,response.clone());
+          return response;
+        });
+      }).catch(function(){      
+        console.log("Service Worker: Fetching online failed, HAALLPPPP!!!");
+        //Do something else with the request (respond with a different cached file)
       })
-    );
-
-
-  });
+    })
+  );
+});
   
-  self.addEventListener("push",(pushing)=>{
-      console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
-  })
-  
+self.addEventListener("push",(pushing)=>{
+    console.log("Service Worker: I received some push data, but because I am still very simple I don't know what to do with it :(");
+})
