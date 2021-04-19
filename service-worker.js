655c51c4568815dfa04df85fe434a1dada9ed885
Alfar414 <AS.cool@live.nl>
2021-04-19 10:43:05 +0200

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,6 @@
 const cacheName="Main_Page";
 const appFiles=[
 
-  "/index.php",
-  "/pages/edit.php",
-  "/pages/email.php",
-  "/pages/folder.php",
-  "/pages/Meetings.php",
-  "/pages/record.php",
-  "/pages/vergeten.php",
-  "/pages/whiteboard.php",
 
 "/manifest.json",
 "/js/script.js",
